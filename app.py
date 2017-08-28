1f354029b9a656403a614563fabcb40dd7df7510
Figmenyaznae <danila2412@gmail.com>
2017-08-28 12:29:55 +0300

    Learned telebot

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
-from flask import Flask, request
-import requests, json
+import telebot
+
 
 BOT_TOKEN = '398858338:AAGMhLrE_eNfLwKOfeFQuLhlEH7g878_fOg'
 URL = 'https://api.telegram.org/bot%s/' % BOT_TOKEN
 MyURL = 'https://fb1488bot.herokuapp.com/'
 
-api = requests.Session()
-set_hook = api.get(URL + 'setWebhook?url=%s' % MyURL)
+bot = telebot.TeleBot(BOT_TOKEN)
 
-app = Flask(__name__)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+        bot.reply_to(message, 'Hello, I\'m still in alpha-test mode, please, don\'t speak to me if you are not my developer.')
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    if request.method=='POST':
-        try:
-            message = json.loads(request.data)['message']
-            api.post(URL + 'sendMessage', data={
-                    'chat_id': message['chat']['id'],
-                    'text': 'You sent me' + message['text']
-                })
-            return 'OK'
-        except:
-            raise Exception(request.data)    
-    else:
-        return 'Hello, I\'m still in alpha-test mode, please, don\'t speak to me if you are not my developer.'
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
